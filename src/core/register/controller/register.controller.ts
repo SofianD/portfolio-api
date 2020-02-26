c11bbe836380c3d847ef47cbc5ed848cce0bc42a
SofianD <doual.sofian@hotmail.com>
2020-02-26 19:31:38 +0100

    #35 create register feature

--- src/core/register/controller/register.controller.ts
+++ src/core/register/controller/register.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { RegisterService } from '../service/register.service';
 
 @Controller('register')
-export class RegisterController {}
+export class RegisterController {
+
+    constructor(
+        private readonly registerService: RegisterService
+    ) {}
+
+    @Post()
+    async register(
+        @Body('userData') userData: any,
+    ) {
+        const result = await this.registerService.createAdmin(userData);
+        console.log(result);
+        return 'ok'
+    }
+}
--- src/core/register/service/register.service.ts
+++ src/core/register/service/register.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from "@nestjs/mongoose";
+import { Admin } from 'src/shared/models/admin.interface';
+import { Model } from "mongoose";
 
 @Injectable()
-export class RegisterService {}
+export class RegisterService {
+
+    constructor(
+        @InjectModel('Admin') private readonly adminModel: Model<Admin>
+    ) {}
+
+    async createAdmin(
+        userData: any
+        ) {
+        const newAdmin = new this.adminModel(userData);
+        console.log(userData);
+        return await newAdmin.save();
+    }
+}
