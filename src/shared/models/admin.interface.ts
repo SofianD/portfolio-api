509eb1e5069f7b338de70426795e81b31b9e2458
SofianD <doual.sofian@hotmail.com>
2020-02-26 14:44:33 +0100

    #39 create admin model

---
+++ src/shared/models/admin.interface.ts
@@ -0,0 +1,17 @@
+import * as mongoose from 'mongoose';
+
+export const AdminSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export interface Admin {
+    email: string;
+    password: string;
+}
