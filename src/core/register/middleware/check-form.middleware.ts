373f265943edd07f3cd112f11aa44036e99abb5e
SofianD <doual.sofian@hotmail.com>
2020-02-26 20:56:42 +0100

    #35  add middleware to check register form

---
+++ src/core/register/middleware/check-form.middleware.ts
@@ -0,0 +1,24 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class CheckFormMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+
+    const userData = req.body.userData;
+    this.checkBodyValue(userData).length === 0 ?
+      next()
+    :
+      res.status(401).json({
+        message: 'Failed request',
+        error: this.checkBodyValue(userData)
+      })
+  }
+
+  checkBodyValue(userData: any) {
+    let errors: string[] = [];
+    if (!userData.email || userData.email.split('@').length !== 2 || userData.email.length < 6) errors.push('Required a valid email');
+    if (!userData.password || userData.password.length < 6) errors.push('Required a valid password');
+    return errors;
+  }
+
+}
