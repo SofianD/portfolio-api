df4e38f1e05173d698aafd8afb18584f6a8aab76
SofianD <doual.sofian@hotmail.com>
2020-02-26 19:29:25 +0100

    #40 create and add new db link

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RegisterModule } from './core/register/register.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
 
 @Module({
   imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://user:5bLXgHFheEOQrowe@cluster0-ohnuj.mongodb.net/test?retryWrites=true&w=majority',
+      { useNewUrlParser: true }
+    ),
     RegisterModule
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+  ]
 })
 export class AppModule {}
