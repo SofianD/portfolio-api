61178fccf621250d3cb1c2a19648530b00798896
SofianD <doual.sofian@hotmail.com>
2020-02-26 13:52:24 +0100

    #35 import Register.module in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RegisterModule } from './core/register/register.module';
 
 @Module({
-  imports: [],
+  imports: [
+    RegisterModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
