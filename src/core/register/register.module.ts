45f28c0836cc00ed125176ea5cd89889a8cee789
SofianD <doual.sofian@hotmail.com>
2020-02-26 19:30:47 +0100

    #39 import admin schema in register module

--- src/core/register/register.module.ts
+++ src/core/register/register.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common';
 import { RegisterController } from './controller/register.controller';
 import { RegisterService } from './service/register.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AdminSchema } from 'src/shared/models/admin.interface';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([{
+            name: 'Admin',
+            schema: AdminSchema
+        }])
+    ],
     controllers: [
         RegisterController
     ],
