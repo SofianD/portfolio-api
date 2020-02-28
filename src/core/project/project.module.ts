25e458519c86b0e2df9c5742ca11bbd7b6e7c836
SofianD <doual.sofian@hotmail.com>
2020-02-28 16:21:46 +0100

    #56 import project components in project.module

--- src/core/project/project.module.ts
+++ src/core/project/project.module.ts
@@ -1,4 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ProjectController } from './controller/project.controller';
+import { ProjectService } from './service/project.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProjectSchema } from 'src/shared/models/project.interface';
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([{
+            name: 'Project',
+            schema: ProjectSchema
+        }])
+    ],
+    controllers: [
+        ProjectController
+    ],
+    providers:[
+        ProjectService
+    ]
+})
 export class ProjectModule {}
