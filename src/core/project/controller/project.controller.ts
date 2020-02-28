feff0c3e09f960720d4de2c6b25ca64a58256128
SofianD <doual.sofian@hotmail.com>
2020-02-28 16:16:21 +0100

    #56 add CRUD functions in project.controller

--- src/core/project/controller/project.controller.ts
+++ src/core/project/controller/project.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, UseGuards, Body, Delete, Put } from '@nestjs/common';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
 
 @Controller('project')
-export class ProjectController {}
+export class ProjectController {
+
+    @Get()
+    async getAllProjects() {
+
+    }
+
+    @Get(':id')
+    async getOneProject(@Param('id') id: string) {
+
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    async createProject(@Body('projectData') projectData: object) {
+
+    }
+
+    @Put(':id')
+    @UseGuards(AuthGuard)
+    async updateProject(@Param('id') id: string) {
+
+    }
+
+    @Delete(':id')
+    @UseGuards(AuthGuard)
+    async deleteProject(@Param('id') id: string) {
+
+    }
+}
