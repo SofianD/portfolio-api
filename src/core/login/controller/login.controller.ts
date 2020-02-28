b355f352f15f436cb08f930f871accc7d9cb60e2
SofianD <doual.sofian@hotmail.com>
2020-02-28 09:24:04 +0100

    #51 assign token when admin is logged

--- src/core/login/controller/login.controller.ts
+++ src/core/login/controller/login.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, HttpException, HttpStatus } from '@nestjs/common';
 import { LoginService } from '../service/login.service';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
 @Controller('login')
 export class LoginController {
@@ -16,13 +17,18 @@ export class LoginController {
         // Check if admin exist
         const result = await this.loginService.login(data.email);
         if (result === null)  throw new HttpException('No account with this email', HttpStatus.NOT_FOUND);
+        
         // Check if password match
         const isAuth = await bcrypt.compare(data.password, result.password);
         if (isAuth === false) throw new HttpException('Wrong password', HttpStatus.FORBIDDEN);
+
         // return JsonWebToken
         return {
             message: 'Authenticated',
-            token: 'False tokeeeeeeeeeen'
+            token: jwt.sign(
+                {_id: result._id},
+                '167CD6DC2E719C1CE671DBAEA8465'
+            )
         };
     }
 }
