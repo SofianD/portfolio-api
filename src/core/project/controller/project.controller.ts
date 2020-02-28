d1cb68851e07174e6ecf2c9d92cc06bb4612767a
SofianD <doual.sofian@hotmail.com>
2020-02-28 18:44:44 +0100

    #56 set up crud functions

--- src/core/project/controller/project.controller.ts
+++ src/core/project/controller/project.controller.ts
@@ -1,34 +1,45 @@
 import { Controller, Get, Param, Post, UseGuards, Body, Delete, Put } from '@nestjs/common';
 import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { ProjectService } from '../service/project.service';
+import { Project } from 'src/shared/models/project.interface';
 
 @Controller('project')
 export class ProjectController {
 
+    constructor(
+        private readonly projectService: ProjectService
+    ) {}
+
     @Get()
     async getAllProjects() {
-
+        return await this.projectService.allProjects();
     }
 
     @Get(':id')
     async getOneProject(@Param('id') id: string) {
-
+        return this.projectService.oneProject(id);
     }
 
     @Post()
     @UseGuards(AuthGuard)
-    async createProject(@Body('projectData') projectData: object) {
-
+    async createProject(
+        @Body('projectData') projectData: Project
+    ): Promise<Project> {
+        return await this.projectService.create(projectData);
     }
 
     @Put(':id')
     @UseGuards(AuthGuard)
-    async updateProject(@Param('id') id: string) {
-
+    async updateProject(
+        @Param('id') id: string,
+        @Body('projectData') project: Project
+    ) {
+        return this.projectService.update(id, project);
     }
 
     @Delete(':id')
     @UseGuards(AuthGuard)
     async deleteProject(@Param('id') id: string) {
-
+        return this.projectService.delete(id);
     }
 }
--- src/core/project/service/project.service.ts
+++ src/core/project/service/project.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Project } from 'src/shared/models/project.interface';
+import { Model } from "mongoose";
 
 @Injectable()
-export class ProjectService {}
+export class ProjectService {
+
+    constructor(
+        @InjectModel('Project') private readonly projectModel: Model<Project>
+    ) {}
+
+    async allProjects() {
+        return await this.projectModel.find().exec();
+    }
+
+    async oneProject(id: string) {
+        return await this.projectModel.findOne({_id: id}).exec();
+    }
+
+    async create(project: Project) {
+        const result =  await new this.projectModel(project).save();
+        const {__v, ...createdProject} = result._doc;
+        return createdProject;
+    }
+
+    async update(id: string, project: Project) {
+        const result = await this.projectModel.findOneAndUpdate(
+            {_id: id},
+            project
+        );
+        return result;
+    }
+
+    async delete(id: string) {
+        const result = await this.projectModel.deleteOne({_id: id});
+        return result;
+    }
+}
