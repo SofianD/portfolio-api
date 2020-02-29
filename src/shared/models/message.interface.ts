0339b6d38239a59fa3940889677b07d701757572
SofianD <doual.sofian@hotmail.com>
2020-02-29 20:25:53 +0100

    #57 add message interface and schema

---
+++ src/shared/models/message.interface.ts
@@ -0,0 +1,32 @@
+import * as mongoose from "mongoose";
+import { Document } from 'mongoose';
+
+export const MessageSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    phone_number: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    sender: {
+        type: String,
+        required: true
+    }
+})
+
+export interface Message {
+    content: string;
+    subject: string;
+    phone_number: string;
+    email: string;
+    sender: string;
+}
