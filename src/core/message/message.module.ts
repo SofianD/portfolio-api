20618adeb26d738034ee43d6f9579178f68dba0d
SofianD <doual.sofian@hotmail.com>
2020-02-29 20:57:14 +0100

    #57 import middleware un message module

--- src/core/message/message.module.ts
+++ src/core/message/message.module.ts
@@ -1,8 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MessageSchema } from 'src/shared/models/message.interface';
 import { MessageController } from './controller/message.controller';
 import { MessageService } from './service/message.service';
+import { CheckFormMiddleware } from "./middleware/check-form.middleware";
+
 
 @Module({
     imports: [
@@ -20,4 +22,15 @@ import { MessageService } from './service/message.service';
         MessageService
     ]
 })
-export class MessageModule {}
+export class MessageModule implements NestModule {
+    configure(consumer: MiddlewareConsumer) {
+        consumer
+            .apply(
+                CheckFormMiddleware
+            )
+            .forRoutes(
+                { path: 'message', method: RequestMethod.POST },
+                { path: 'message', method: RequestMethod.PUT }
+            );
+    }
+}
