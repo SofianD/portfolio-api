7b8ac1cc78452377f919ddbf5d7f424afcf6bebb
SofianD <doual.sofian@hotmail.com>
2020-02-29 20:30:01 +0100

    #57 add message controller (crud functions)

---
+++ src/core/message/controller/message.controller.ts
@@ -0,0 +1,59 @@
+import { Controller, Get, Body, Param, HttpException, HttpStatus, Post, UseGuards, Put, Delete } from '@nestjs/common';
+import { MessageService } from '../service/message.service';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { Message } from 'src/shared/models/message.interface';
+
+
+@Controller('message')
+export class MessageController {
+    constructor(
+        private readonly msgService: MessageService
+    ) {}
+
+    @Get()
+    async getAllMessages() {
+        return await this.msgService.allMessages();
+    }
+
+    @Get(':id')
+    async getOneSMessage(
+        @Param('id') id: string
+    ) {
+        const result = await this.msgService.oneMessage(id);
+        if (result === null) throw new HttpException('NOT FOUND', HttpStatus.NOT_FOUND);
+        return result;
+    }
+
+    @Post()
+    @UseGuards(AuthGuard)
+    async createMessage(
+        @Body('message') message: Message
+    ):Promise<Message> {
+        return await this.msgService.create(message);
+    }
+
+    @Put(':id')
+    @UseGuards(AuthGuard)
+    async updateMessage(
+        @Param('id') id: string,
+        @Body('message') message: Message
+    ) {
+        return await this.msgService.update(id, message) === 0
+        ?
+        {message: 'No Changes made'}
+        :
+        await this.msgService.oneMessage(id)
+        ;
+    }
+
+    @Delete(':id')
+    @UseGuards(AuthGuard)
+    async deleteMessage(
+        @Param('id') id: string
+    ) {
+        const result = await this.msgService.delete(id);
+        if (result.n === 0) throw new HttpException('MESSAGE NOT FOUND', HttpStatus.NOT_FOUND);
+        if (result.deletedCount === 0) return { message:'Deletion failed' };
+        return {message:'Deletion was successfull'};
+    }
+}
