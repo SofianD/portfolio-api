277c5f15e6e0c41172eb9b2681b341e40e8f6174
SofianD <doual.sofian@hotmail.com>
2020-02-29 16:32:23 +0100

    #70 add crud in framework service

---
+++ src/core/framework/service/framework.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Framework } from 'src/shared/models/framework.interface';
+import { Model } from "mongoose";
+
+@Injectable()
+export class FrameworkService {
+    constructor(
+        @InjectModel('Framework') private readonly fwModel: Model<Framework>
+    ) {}
+
+    async allFrameworks() {
+        return await this.fwModel.find().exec();
+    }
+
+    async oneFramework(id: string) {
+        try {
+            const result = await this.fwModel.findOne({_id: id});
+            const {__v, ...createdFramework} = result._doc;
+            return createdFramework;
+        } catch (error) {
+            return null;
+        }
+    }
+    async create(fwData: Framework) {
+        const result = await new this.fwModel(fwData).save();
+        const {__v, ...createdFramework} = result._doc;
+        return createdFramework;
+    }
+
+    async update(
+        id: string,
+        framework: Framework
+    ) {
+        const result = await this.fwModel.updateOne(
+            {_id: id},
+            framework
+        );
+        return result.nModified;
+    }
+
+    async delete(id: string) {
+        return this.fwModel.deleteOne({_id: id});
+    }
+}
