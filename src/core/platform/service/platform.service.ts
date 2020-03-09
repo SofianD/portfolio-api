4179db1494b3a9c8df88e28fd81c19aba0a996aa
SofianD <doual.sofian@hotmail.com>
2020-03-09 17:06:16 +0100

    #100 add crud functions in platform service

--- src/core/platform/service/platform.service.ts
+++ src/core/platform/service/platform.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Platform } from 'src/shared/models/platform.interface';
+import { Model } from "mongoose";
 
 @Injectable()
-export class PlatformService {}
+export class PlatformService {
+    constructor(
+        @InjectModel('Platform') private readonly platformModel: Model<Platform>
+    ) {}
+
+    async getAllPlatforms() {
+        return await this.platformModel.find().exec();
+    }
+
+    async getOnePlatform(id: string) {
+        try {
+            
+            const {__v, ...platform} = (await this.platformModel.findOne({_id: id}))._doc;
+            return platform;
+        } catch (error) {
+            return null;
+        }
+    }
+
+    async create(platform: Platform) {
+        const {__v, ...createdPlatform} = (await new this.platformModel(platform).save())._doc;
+        return createdPlatform;
+    }
+
+    async delete(id :string) {
+        return await this.platformModel.deleteOne({_id: id});
+    } 
+}
