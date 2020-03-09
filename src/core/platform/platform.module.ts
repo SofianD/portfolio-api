a2f0af1436c1f07d102948f4e67b127e14652951
SofianD <doual.sofian@hotmail.com>
2020-03-09 16:53:02 +0100

    #100 setup platform module

--- src/core/platform/platform.module.ts
+++ src/core/platform/platform.module.ts
@@ -1,4 +1,24 @@
 import { Module } from '@nestjs/common';
+import { FrameworkController } from '../framework/controller/framework.controller';
+import { FrameworkService } from '../framework/service/framework.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FrameworkSchema } from 'src/shared/models/framework.interface';
+import { SkillSchema } from 'src/shared/models/skill.interface';
 
-@Module({})
+@Module({
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: 'Framework',
+                schema: FrameworkSchema
+            }
+        ])
+    ],
+    controllers: [
+        FrameworkController
+    ],
+    providers: [
+        FrameworkService
+    ]
+})
 export class PlatformModule {}
