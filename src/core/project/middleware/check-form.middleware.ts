2cd90e922e00913f0e304e0c0e76f5aa9ad92cec
SofianD <doual.sofian@hotmail.com>
2020-03-30 16:43:13 +0200

    #56 fixed project form middleware

--- src/core/project/middleware/check-form.middleware.ts
+++ src/core/project/middleware/check-form.middleware.ts
@@ -4,15 +4,14 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 export class CheckFormMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     const project = req.body.project;
-    
-    this.checkBodyValue(project).length === 0
+    this.checkBodyValue(project).length > 0
     ?
-    next()
-    :
-    res.status(401).json({
+    res.status(400).json({
       message: 'BAD REQUEST',
       error: this.checkBodyValue(project)
-    });
+    })
+    :
+    next();
   }
 
   checkBodyValue(project: any) {
@@ -20,16 +19,16 @@ export class CheckFormMiddleware implements NestMiddleware {
     
     // Replace this by mongoose schema validation
     if (
-      !project.name || project.name > 5
+      project.name.length < 5
     ) errors.push('Required a valid project name');
     if (
-      !project.description || project.description > 5
+      project.description.length < 5
     ) errors.push('Required a valid project description');
     if (
       !project.created_date
     ) errors.push('Required a valid creation date');
     if (
-      !project.images || project.images.length > 10
+      project.images.length > 10
     ) errors.push('10 images per project max');
     if (
       !project.skills
